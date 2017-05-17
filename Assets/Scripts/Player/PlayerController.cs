b0e66595d97f71f2d002b1c404ece6e9e3bc852e
grubbling-fopdoodle <arkenvoss.dk@gmail.com>
2017-05-18 00:20:37 +0100

    movement tweaks

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("PlayerMove"))
+        if (Input.GetButton("PlayerMove"))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
