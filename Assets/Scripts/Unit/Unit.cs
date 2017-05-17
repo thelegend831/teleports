67041420b7370755ff7f5519e56039700b2a330c
grubbling-fopdoodle <arkenvoss.dk@gmail.com>
2017-05-17 23:03:22 +0100

    The player now rotates! Player prefab and material fixed

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
     public int hp_, damage_;
-    public float attackRange_, attackCooldown_, moveSpeed_;
+    public float attackRange_, attackCooldown_, moveSpeed_, rotationSpeed_;
 
     //pathfinding
     public Vector3 moveDest_;
     bool isMoving_;
+    
+    public Quaternion rotationTarget_, rotationOrigin_;
+    bool isRotating_;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         hp_ = 100;
         damage_ = 25;
         attackRange_ = 1;
         attackCooldown_ = 1;
         moveSpeed_ = 1;
+        rotationSpeed_ = moveSpeed_;
         isMoving_ = false;
 	}
 	
@@ -40,13 +44,23 @@ public class Unit : MonoBehaviour {
             }
 
             transform.position += offset;
-
+            if (isRotating_)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationTarget_, dTime * rotationSpeed_ * 360);
+                if(transform.rotation == rotationTarget_)
+                {
+                    isRotating_ = false;
+                }
+            }
         }
 	}
 
     public void moveTo(Vector3 moveDest)
     {
         moveDest_ = moveDest;
+        rotationTarget_ = Quaternion.LookRotation(moveDest_ - transform.position);
+        rotationOrigin_ = transform.rotation;
         isMoving_ = true;
+        isRotating_ = true;
     }
 }
