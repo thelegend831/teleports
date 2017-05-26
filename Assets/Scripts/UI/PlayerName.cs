84e74a2a2155ea93fc1fff4f78db71299e2ba425
grubbling-fopdoodle <arkenvoss.dk@gmail.com>
2017-05-26 20:13:09 +0100

    Added Home scene with basic UI, builds on Android

---
+++ Assets/Scripts/UI/PlayerName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class PlayerName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text text = gameObject.GetComponent<Text>();
+        text.text = GlobalData.instance.playerData_.name_;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
