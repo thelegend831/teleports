47f9f2ab693b808253dc69262b56035cf52343ab
Dominik__ <arkenvoss.dk@gmail.com>
2017-07-22 23:35:03 +0100

    Menu system

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    //private variables
+    private GameObject instantiatedObject;
+    private bool isOpen, isActive;
+
+    //inspector variables
+    public string prefabPath;
+    public MenuController.MenuType menuType;
+    public bool disableMenusUnder; //menu will be closed when enabled back
+    [Tooltip("will parent the menu to the MainCanvas prefab")]
+    public bool useMainCanvas;
+
+    //public functions
+    public void Open()
+    {
+        if(!IsOpen)
+        {
+            if (instantiatedObject == null)
+            {
+                if (useMainCanvas)
+                {
+                    instantiatedObject = Instantiate(Resources.Load(MenuController.MainCanvasPrefabPath), transform) as GameObject;
+                    GameObject temp = Instantiate(Resources.Load(prefabPath), instantiatedObject.transform) as GameObject;
+                }
+                else
+                {
+                    instantiatedObject = Instantiate(Resources.Load(prefabPath), transform) as GameObject;
+                }
+            }
+            else
+            {
+                instantiatedObject.SetActive(true);
+            }
+            IsOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        if (IsOpen && instantiatedObject != null)
+        {
+            Destroy(instantiatedObject);
+            instantiatedObject = null;
+            IsOpen = false;
+        }
+    }
+
+    public void Show()
+    {
+        if (IsOpen && !isActive && instantiatedObject != null)
+        {
+            instantiatedObject.SetActive(true);
+            isActive = true;
+        }
+    }
+
+    public void Hide()
+    {
+        if(IsOpen && isActive && instantiatedObject != null)
+        {
+            instantiatedObject.SetActive(false);
+            isActive = false;
+        }
+    }
+
+    //properties
+    public bool IsOpen
+    {
+        get { return isOpen; }
+        private set
+        {
+            isOpen = value;
+            isActive = value;
+        }
+    }
+    public bool IsActive
+    {
+        get { return isActive; }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//singleton controlling menu stack
+public class MenuController : MonoBehaviour {
+
+    private static MenuController instance;
+
+    private Menu[] menus;
+    private Stack<Menu> menuStack;
+
+	public enum MenuType { CreateCharacter, ChooseCharacter, Home, Count };
+    public Menu[] menuInspectorLinks;
+    public MenuType startMenu;
+    public string mainCanvasPrefabPath;
+
+
+    //unity event functions
+    void Awake()
+    {
+        instance = this;
+
+        menus = new Menu[(int)MenuType.Count];
+        menuStack = new Stack<Menu>();
+
+        foreach(Menu menu in menuInspectorLinks)
+        {
+            menus[(int)menu.menuType] = menu;
+        }
+
+        OpenMenu(startMenu);
+    }
+
+    void OnDestroy()
+    {
+        CloseAll();
+        instance = null;
+    }
+
+    //public functions
+    public void OpenMenu(MenuType menuType)
+    {
+        Menu menu = menus[(int)menuType];
+        if (menu != null && !menu.IsOpen)
+        {
+            if (menu.disableMenusUnder)
+            {
+                HideAll();
+            }
+            menuStack.Push(menu);
+            menu.Open();
+        }
+    }
+
+    public void CloseMenu(MenuType menuType)
+    {
+        Menu menu = menus[(int)menuType];
+        if(menu == menuStack.Peek())
+        {
+            CloseTopMenu();
+            if (menu.disableMenusUnder)
+            {
+                ShowTopMenus();
+            }
+        }
+    }
+
+    public void CloseTopMenu()
+    {
+        if (menuStack.Count != 0)
+        {
+            Menu menu = menuStack.Pop();
+            menu.Close();
+        }
+    }
+
+    //protected functions
+    protected void CloseAll()
+    {
+        while(menuStack.Count > 0)
+        {
+            CloseTopMenu();
+        }
+    }
+
+    protected void HideAll()
+    {
+        foreach (Menu menu in menuStack)
+        {
+            menu.Hide();
+        }
+    }
+
+    //will close all menus with disableMenusUnder set to false (close popups)
+    protected void ShowTopMenus()
+    {
+        while(menuStack.Count != 0)
+        {
+            Menu menu = menuStack.Peek();
+            if (!menu.disableMenusUnder)
+            {
+                CloseTopMenu();
+            }
+            else
+            {
+                menu.Show();
+                break;
+            }
+        }
+    }
+
+    //properties
+    public static string MainCanvasPrefabPath
+    {
+        get { return instance.mainCanvasPrefabPath; }
+    }
+
+
+}
