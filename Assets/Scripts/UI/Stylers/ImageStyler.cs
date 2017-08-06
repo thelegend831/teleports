389aec67c913d4c368f0bab1bfdb9b4f188e1514
Dominik__ <arkenvoss.dk@gmail.com>
2017-08-06 23:52:09 +0200

    ImageStyler

---
+++ Assets/Scripts/UI/Stylers/ImageStyler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageStyler : LoadableBehaviour {
+
+    public Stylesheet.ColorPreset color;
+
+    public override void LoadData()
+    {
+        Image image = gameObject.GetComponent<Image>();
+        image.color = MainData.CurrentStylesheet.GetColorPreset(color);
+    }
+}
