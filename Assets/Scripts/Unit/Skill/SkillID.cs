a7107e04b5a637f89a65a7187dea6f963ca5ac64
Dominik__ <arkenvoss.dk@gmail.com>
2017-09-27 01:41:54 +0200

    SkillID now can use both tree position and string IDs

--- Assets/Scripts/Unit/Skill/SkillID.cs
+++ Assets/Scripts/Unit/Skill/SkillID.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//class used as a skill identifier
+//you can use skill's position in the skill tree or its name
 [System.Serializable]
-public struct SkillID {
+public class SkillID {
 
     public byte
         treeID,
         branchID,
         elementID;
+
+    public string skillName = string.Empty;
+
+    public bool UsesString()
+    {
+        return skillName.Length > 0;
+    }
 }
