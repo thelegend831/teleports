c92ca5bb0bf0bd41c7d659e0639f6ddc8b955c3a
Dominik__ <arkenvoss.dk@gmail.com>
2017-09-27 18:01:02 +0200

    StunnedState, DeadState added

---
+++ Assets/Scripts/Unit/ActionState/DeadState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadState : ActionState {
+
+    protected Unit lastAttacker = null;
+
+	public DeadState(Unit unit) : base(unit)
+    {
+        Reset();
+    }
+
+    public override void Start()
+    {
+        if (!IsActive)
+        {
+            isActive = true;
+            if (lastAttacker != null)
+            {
+                Xp xp = lastAttacker.gameObject.GetComponent<Xp>();
+                if (xp != null)
+                {
+                    xp.receiveXp((1000 * unit.unitData.Level));
+                }
+            }
+        }
+    }
+
+    public override void Update(float dTime)
+    {
+        
+    }
+
+    public override void Reset()
+    {
+        lastAttacker = null;
+        isActive = false;
+    }
+
+    public Unit LastAttacker
+    {
+        get { return lastAttacker; }
+        set { lastAttacker = value; }
+    }
+}
---
+++ Assets/Scripts/Unit/ActionState/StunnedState.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunnedState : ActionState {
+
+    protected float stunTime;
+
+    public StunnedState(Unit unit): base(unit)
+    {
+        Reset();
+    }
+
+    public override void Start()
+    {
+        if(stunTime > 0)
+        {
+            isActive = true;
+        }
+    }
+
+    public override void Update(float dTime)
+    {
+        if(isActive && !IsBlocked)
+        {
+            stunTime -= dTime;
+            if(stunTime <= 0)
+            {
+                Reset();
+            }
+        }
+    }
+
+    public override void Reset()
+    {
+        stunTime = 0;
+        isActive = false;
+    }
+
+    public float StunTime
+    {
+        get { return stunTime; }
+        set
+        {
+            if(value > stunTime)
+            {
+                stunTime = value;
+                Start();
+            }
+        }
+    }
+}
