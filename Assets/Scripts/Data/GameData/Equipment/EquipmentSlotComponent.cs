dfeb2ea9d51d5ec3ef3050cba1259b0297530d73
Dominik__ <arkenvoss.dk@gmail.com>
2017-10-08 17:26:05 +0200

    EquipmentSlotComponent

---
+++ Assets/Scripts/Data/GameData/Equipment/EquipmentSlotComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSlotComponent : MonoBehaviour {
+
+    [SerializeField]
+    private EquipmentSlot slotType;
+
+    public EquipmentSlot SlotType{
+        get { return slotType; }
+    }
+}
