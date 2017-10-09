fab46a536961311a56d2e9d2c2dca7f432fbc11c
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-09 14:40:30 +0200

    domis napraw moje booooole

---
+++ Assets/Laboratorium Milowicza/Skrypciatka/Foot_AnimationSynchronize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foot_AnimationSynchronize : MonoBehaviour {
+
+    public ParticleSystem L_Foot_ParticleSystem;
+    public ParticleSystem R_Foot_ParticleSystem;
+	
+	void R_FootStomp () {
+        R_Foot_ParticleSystem = GetComponent<ParticleSystem>();
+        R_Foot_ParticleSystem.Play();
+    }
+    void L_FootStomp()
+    {
+        L_Foot_ParticleSystem = GetComponent<ParticleSystem>();
+        L_Foot_ParticleSystem.Play();
+    }
+}
