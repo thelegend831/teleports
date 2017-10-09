f5f99663311b2480981d0a575f26177e4cd84c23
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-09 18:24:30 +0200

    DeathKnight Foot Stomp

--- Assets/Laboratorium Milowicza/Enemies/DeathKnight/DeathKnight_FootParticlesSynchronize.cs
+++ Assets/Laboratorium Milowicza/Enemies/DeathKnight/DeathKnight_FootParticlesSynchronize.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Foot_AnimationSynchronize : MonoBehaviour {
+public class DeathKnight_FootParticlesSynchronize : MonoBehaviour {
 
     public ParticleSystem L_Foot_ParticleSystem;
     public ParticleSystem R_Foot_ParticleSystem;
 	
 	void R_FootStomp () {
-        R_Foot_ParticleSystem = GetComponent<ParticleSystem>();
         R_Foot_ParticleSystem.Play();
     }
     void L_FootStomp()
     {
-        L_Foot_ParticleSystem = GetComponent<ParticleSystem>();
         L_Foot_ParticleSystem.Play();
     }
 }
