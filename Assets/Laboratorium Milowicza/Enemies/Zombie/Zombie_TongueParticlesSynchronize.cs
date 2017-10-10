25c5aa2eb70adb55dc2a6bd55fca809c3401034f
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-10 22:58:05 +0200

    commit na koniec dnia

---
+++ Assets/Laboratorium Milowicza/Enemies/Zombie/Zombie_TongueParticlesSynchronize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie_TongueParticlesSynchronize : MonoBehaviour {
+
+    public ParticleSystem TongueParticleSystem;
+
+    void Lick () {
+        TongueParticleSystem.Play();
+    }
+}
