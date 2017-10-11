5b6d90c9265e35a335fc23a8d660d131a6a0f7e7
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-11 14:58:33 +0200

    reaper eyecandy

---
+++ Assets/Laboratorium Milowicza/Enemies/Reaper/Reaper_Terrify_Animation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper_Terrify_Animation : MonoBehaviour {
+
+    public ParticleSystem TerrifyParticleSystem;
+
+ 
+    void Terrify()
+    {
+        TerrifyParticleSystem.Play();
+    }
+}
