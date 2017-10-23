15cce86c8b958e4d70903993245079131778d577
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-23 16:49:23 +0200

    first sound

---
+++ Assets/Laboratorium Milowicza/Enemies/Reaper/Reaper_Scripts/Reaper_Sound_Animation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper_Sound_Animation : MonoBehaviour {
+
+    public AudioClip[] Reaper_Sounds;
+    public AudioSource Reaper_Audio;
+
+
+    void ScytheAttack() {
+        AudioClip clip = Reaper_Sounds[0];
+        Reaper_Audio.clip = clip;
+        Reaper_Audio.Play();
+    }
+}
