9eccb9faf722f57471cc4d94cc9be13a9b8c7aee
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-25 17:20:01 +0200

    kolejne dzwieki

--- Assets/Laboratorium Milowicza/Enemies/Devourer/Devourer_Scripts/Devourer_Audio_Animation.cs
+++ Assets/Laboratorium Milowicza/Enemies/Devourer/Devourer_Scripts/Devourer_Audio_Animation.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class Devourer_Audio_Animation : MonoBehaviour {
 
-    public AudioSource Devourer_Audio;
+    public AudioSource Devourer_Body_Audio;
+    public AudioSource Devourer_Enviroment_Audio;
     public AudioClip[] Devourer_Clips;
 
     void Surface_Roar() {
         AudioClip clip = Devourer_Clips[0];
-        Devourer_Audio.clip = clip;
-        Devourer_Audio.Play();
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
+    }
+
+    void Surface() {
+        AudioClip clip = Devourer_Clips[2];
+        Devourer_Enviroment_Audio.clip = clip;
+        Devourer_Enviroment_Audio.Play();
     }
 
     void Death_Roar()
     {
         AudioClip clip = Devourer_Clips[1];
-        Devourer_Audio.clip = clip;
-        Devourer_Audio.Play();
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
+    }
+
+    void Death_Thump()
+    {
+        AudioClip clip = Devourer_Clips[4];
+        Devourer_Enviroment_Audio.clip = clip;
+        Devourer_Enviroment_Audio.Play();
+    }
+
+    void Dive() {
+        AudioClip clip = Devourer_Clips[3];
+        Devourer_Enviroment_Audio.clip = clip;
+        Devourer_Enviroment_Audio.Play();
+    }
+
+    void Hit()
+    {
+        AudioClip clip = Devourer_Clips[5];
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
     }
 }
