09315420c49dc9a791c8cb54ec86cdd6866cdda7
SkatingKwiatek <olinadgisel@gmail.com>
2017-10-27 15:23:24 +0200

    znowu robie se przerwe, moze juz na caly dzien, nie wiem

--- Assets/Laboratorium Milowicza/Enemies/Devourer/Devourer_Scripts/Devourer_Audio_Animation.cs
+++ Assets/Laboratorium Milowicza/Enemies/Devourer/Devourer_Scripts/Devourer_Audio_Animation.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Devourer_Audio_Animation : MonoBehaviour {
+public class Devourer_Audio_Animation : MonoBehaviour
+{
 
     public AudioSource Devourer_Body_Audio;
     public AudioSource Devourer_Enviroment_Audio;
     public AudioClip[] Devourer_Clips;
 
-    void Surface_Roar() {
+    void Surface_Roar()
+    {
         AudioClip clip = Devourer_Clips[0];
         Devourer_Body_Audio.clip = clip;
         Devourer_Body_Audio.Play();
     }
 
-    void Surface() {
+    void Surface()
+    {
         AudioClip clip = Devourer_Clips[2];
         Devourer_Enviroment_Audio.clip = clip;
         Devourer_Enviroment_Audio.Play();
@@ -34,7 +37,8 @@ public class Devourer_Audio_Animation : MonoBehaviour {
         Devourer_Enviroment_Audio.Play();
     }
 
-    void Dive() {
+    void Dive()
+    {
         AudioClip clip = Devourer_Clips[3];
         Devourer_Enviroment_Audio.clip = clip;
         Devourer_Enviroment_Audio.Play();
@@ -46,8 +50,31 @@ public class Devourer_Audio_Animation : MonoBehaviour {
         Devourer_Body_Audio.clip = clip;
         Devourer_Body_Audio.Play();
     }
-    void Breath() {
+    void Breath()
+    {
         AudioClip clip = Devourer_Clips[6];
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
+    }
+    void Step()
+    {
+        AudioClip clip = Devourer_Clips[7];
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
+    }
+    void StepDeath()
+    {
+        AudioClip clip = Devourer_Clips[7];
+        Devourer_Enviroment_Audio.clip = clip;
+        Devourer_Enviroment_Audio.Play();
+    }
+    void TailStrikeAudio() {
+        AudioClip clip = Devourer_Clips[8];
+        Devourer_Body_Audio.clip = clip;
+        Devourer_Body_Audio.Play();
+    }
+    void TeethClack() {
+        AudioClip clip = Devourer_Clips[9];
         Devourer_Body_Audio.clip = clip;
         Devourer_Body_Audio.Play();
     }
