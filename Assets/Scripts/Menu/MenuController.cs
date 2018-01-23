77c695af8a66ad4bc7a96a123a52824c1bf493b8
Dominik__ <arkenvoss.dk@gmail.com>
2018-01-23 14:11:41 +0100

    Inventory Menu added to main game

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -8,7 +8,7 @@ using System;
 [CreateAssetMenu(fileName = "menuController", menuName = "Menu/Controller")]
 public class MenuController : ScriptableObject
 {
-    public enum MenuType { CreateCharacter, ChooseCharacter, Popup, TestA, TestB, Home, DialogWindow, Count };
+    public enum MenuType { CreateCharacter, ChooseCharacter, Popup, TestA, TestB, Home, DialogWindow, Count, Inventory };
 
     private static MenuController instance;
 
