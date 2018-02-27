6ee3d7b2248af51bd087fd80a0822f6655617139
Dominik__ <arkenvoss.dk@gmail.com>
2018-02-27 14:00:45 +0100

    SoundController

---
+++ Assets/Scripts/Utils/GraphicsControllers/SoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clips;
+    private AudioSource audioSource;
+
+    public void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play(int id)
+    {
+        if (id >= clips.Length)
+        {
+            Debug.LogWarning("Trying to play sound clip with invalid id");
+            return;
+        }
+        audioSource.clip = clips[id];
+        audioSource.Play();
+    }
+}
