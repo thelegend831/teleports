4dcc0f238ef66e33c392e18c5eb9e606b8780c6c
Dominik__ <arkenvoss.dk@gmail.com>
2019-02-08 16:56:49 +0100

    Data script folders renamed

---
+++ Assets/Scripts/Main/UISystem/Widgets/TextWidget/TextWidget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Text = TMPro.TextMeshProUGUI;
+
+public class TextWidget : MonoBehaviour
+{
+
+    private Text text;
+
+
+}
---
+++ Assets/Scripts/Menu/Atributes/AttributeUpgraderUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Text = TMPro.TextMeshProUGUI;
+
+public class AttributeUpgraderUI : MonoBehaviour
+{
+
+    private UnitAttributesData.AttributeType attributeType;
+
+    [SerializeField] private Text nameText;
+    [SerializeField] private Text valueText;
+}
