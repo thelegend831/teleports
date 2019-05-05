81487f421ea60b801b6ab1d2e7ff6a30842cb812
Dominik__ <arkenvoss.dk@gmail.com>
2019-05-05 22:34:44 +0200

    Bug-31: Enemy indicators are active even for some dead enemies

--- Assets/Scripts/UI/World/EnemyIndicator.cs
+++ Assets/Scripts/UI/World/EnemyIndicator.cs
@@ -7,6 +7,7 @@ using Teleports.Utils;
 public class EnemyIndicator : MonoBehaviour {
 
     GameObject enemy, indicator;
+    Unit enemyUnit;
     Camera cam;   
 
 	void Awake () {
@@ -21,50 +22,56 @@ public class EnemyIndicator : MonoBehaviour {
 	
 	void Update ()
     {
-        if (enemy != null)
+        if (enemy == null || enemyUnit.DeadState.IsActive)
         {
-            Vector3 enemyPos; //enemy position in viewport
-
-            enemyPos = cam.WorldToViewportPoint(enemy.transform.position);
-
-            if (enemyPos.z > 0 && enemyPos.y > 0 && enemyPos.y < 1 && enemyPos.x > 0 && enemyPos.x < 1)
-            {
-                MakeInvisible();
-                return;
-            }
-            else
-            {
-                MakeVisible();
-            }
-
-            if (enemyPos.z < 0) enemyPos *= -1f;
-
-            float x = enemyPos.x - 0.5f;
-            float y = enemyPos.y - 0.5f;
-
-            float scale = 0.5f / Mathf.Max(Mathf.Abs(x), Mathf.Abs(y));
-
-            Vector3 indicatorPos = cam.ViewportToScreenPoint((enemyPos - (Vector3.one * 0.5f)) * scale + Vector3.one * 0.5f);
-            indicatorPos.z = 2;
-
-            indicator.transform.position = indicatorPos;
-            float angle = Mathf.Asin(x / Mathf.Sqrt(x * x + y * y)) * Mathf.Rad2Deg;
-            if (y > 0) angle = 180 - angle;
-            indicator.transform.rotation = Quaternion.Euler(0, 0, angle); 
+            MakeInvisible();
+            return;
         }
+
+        Vector3 enemyPos; //enemy position in viewport
+
+        enemyPos = cam.WorldToViewportPoint(enemy.transform.position);
+
+        if (enemyPos.z > 0 && enemyPos.y > 0 && enemyPos.y < 1 && enemyPos.x > 0 && enemyPos.x < 1)
+        {
+            MakeInvisible();
+            return;
+        }
+        else
+        {
+            MakeVisible();
+        }
+
+        if (enemyPos.z < 0) enemyPos *= -1f;
+
+        float x = enemyPos.x - 0.5f;
+        float y = enemyPos.y - 0.5f;
+
+        float scale = 0.5f / Mathf.Max(Mathf.Abs(x), Mathf.Abs(y));
+
+        Vector3 indicatorPos = cam.ViewportToScreenPoint((enemyPos - (Vector3.one * 0.5f)) * scale + Vector3.one * 0.5f);
+        indicatorPos.z = 2;
+
+        indicator.transform.position = indicatorPos;
+        float angle = Mathf.Asin(x / Mathf.Sqrt(x * x + y * y)) * Mathf.Rad2Deg;
+        if (y > 0) angle = 180 - angle;
+        indicator.transform.rotation = Quaternion.Euler(0, 0, angle); 
+        
 	}
 
     public void SetEnemy(GameObject enemy)
     {
         this.enemy = enemy;
+        enemyUnit = enemy.GetComponent<Unit>();
+        Debug.Assert(enemyUnit != null);
     }
 
-    void MakeInvisible()
+    private void MakeInvisible()
     {
         indicator.MakeInvisible();
     }
 
-    void MakeVisible()
+    private void MakeVisible()
     {
         indicator.MakeVisible();
     }
