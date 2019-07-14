d55c1492873393107e9eefc8ee6ee0d8ef055d86
Dominik Kowalczyk <arkenvoss@live.com>
2019-07-15 00:39:41 +0200

    Winsock2 initialized

--- sisyphus/Client/src/main.cpp
+++ sisyphus/Client/src/main.cpp
@@ -1,3 +1,10 @@
-int main() {
+#include <winsock2.h>
+#include <stdexcept>
 
+int main() {
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+		throw std::runtime_error("Winsock initialization failed!");
+	}
 }
