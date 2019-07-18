c8f9b5c7be5aa297edc86edc1a8cc3ba091f463c
Dominik Kowalczyk <arkenvoss@live.com>
2019-07-18 16:06:34 +0200

    vkDevice creation

--- sisyphus/Renderer/src/renderer_main.cpp
+++ sisyphus/Renderer/src/renderer_main.cpp
@@ -2,6 +2,28 @@
 #include <exception>
 #include <iostream>
 
+
+void InspectDevice(const vk::PhysicalDevice& physicalDevice) {
+	auto queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+	std::cout << "Queue Families:\n";
+	int index = 1;
+	for (auto&& props : queueFamilyProperties) {
+		auto flags = props.queueFlags;
+		std::cout << "\t#" << index << ": " << vk::to_string(flags) << " Count: " << props.queueCount << std::endl;
+		index++;
+	}
+}
+
+int FindGraphicsQueueFamilyIndex(const vk::PhysicalDevice& physicalDevice) {
+	auto queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+	for (int i = 0; i < queueFamilyProperties.size(); i++) {
+		if (queueFamilyProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main() {
 	try {
 		vk::ApplicationInfo applicationInfo(
@@ -18,6 +40,33 @@ int main() {
 		);
 
 		auto instance = vk::createInstanceUnique(instanceCreateInfo);
+
+		auto physicalDevices = instance->enumeratePhysicalDevices();
+		if (physicalDevices.empty()) {
+			throw::std::runtime_error("No physical devices supporting Vulkan");
+		}
+
+		auto& chosenDevice = physicalDevices[0];
+		std::cout << "Creating a Vulkan Device from " << chosenDevice.getProperties().deviceName << std::endl;
+		InspectDevice(chosenDevice);
+
+		int queueFamilyIndex = FindGraphicsQueueFamilyIndex(chosenDevice);
+		if (queueFamilyIndex == -1) {
+			throw std::runtime_error("Graphics queue not found in the device");
+		}
+		vk::DeviceQueueCreateInfo deviceQueueCreateInfo(
+			{},
+			queueFamilyIndex,
+			1
+		);
+
+		vk::DeviceCreateInfo deviceCreateInfo(
+			{},
+			1,
+			&deviceQueueCreateInfo
+		);
+
+		auto device = chosenDevice.createDeviceUnique(deviceCreateInfo);
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
