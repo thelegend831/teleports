96d6dc6ff8b16efa5d5eec2fb75311c0632c7f81
Dominik Kowalczyk <arkenvoss@live.com>
2019-07-18 15:05:01 +0200

    Vulkan instance creation

--- sisyphus/Renderer/src/renderer_main.cpp
+++ sisyphus/Renderer/src/renderer_main.cpp
@@ -1,5 +1,25 @@
-#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.hpp>
+#include <exception>
+#include <iostream>
 
 int main() {
+	try {
+		vk::ApplicationInfo applicationInfo(
+			"Vulkan App",
+			VK_MAKE_VERSION(1, 0, 0),
+			"Sisyphus",
+			VK_MAKE_VERSION(1, 0, 0),
+			VK_API_VERSION_1_0
+		);
 
+		vk::InstanceCreateInfo instanceCreateInfo(
+			{},
+			&applicationInfo
+		);
+
+		auto instance = vk::createInstanceUnique(instanceCreateInfo);
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 }
