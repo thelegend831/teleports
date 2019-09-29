81f911fc6ffbaef7e1ccc372cfc03639529c4098
Dominik Kowalczyk <arkenvoss@live.com>
2019-09-29 19:58:17 +0200

    Asset test

--- sisyphus/AssetManagement_Test/src/Test_Asset.cpp
+++ sisyphus/AssetManagement_Test/src/Test_Asset.cpp
@@ -1,6 +1,29 @@
 #include "catch.hpp"
 #include "AssetManagement/Asset.h"
+#include "Utils\FilesystemUtils.h"
+#include <fstream>
+using namespace AssetManagement;
 
 TEST_CASE("Asset") {
+	Path dirPath = fs::current_path();
+	dirPath /= "temp";
+	fs::remove_all(dirPath);
+	fs::create_directories(dirPath);
+	Path assetPath = dirPath / "asset.asset";
+	Asset asset(assetPath);
+	Path metaPath = assetPath;
+	metaPath += ".meta";
+	REQUIRE(fs::exists(metaPath));
+	auto id = asset.GetId();
 
+	Asset asset2(assetPath);
+	REQUIRE(id == asset2.GetId());
+
+	std::ofstream file;
+	file.open(metaPath, std::ios::trunc);
+	REQUIRE((file.good() && file.is_open()));
+	file << "Random stuff";
+
+	auto lambda = [assetPath]() {return Asset(assetPath); };
+	REQUIRE_THROWS(lambda());
 }
