7864ed4ae9b2584032c31c048447b54f2db64bed
Dominik Kowalczyk <arkenvoss@live.com>
2019-09-30 19:26:03 +0200

    AssetManager logging

--- sisyphus/AssetManagement/src/AssetManager.cpp
+++ sisyphus/AssetManagement/src/AssetManager.cpp
@@ -1,10 +1,26 @@
 #include "AssetManager.h"
+#include "Utils\Logger.h"
 
 namespace AssetManagement {
 	AssetManager::AssetManager(Path inAssetDir):
 		mainAssetDir(inAssetDir)
 	{
 		InitAssets();
+	}
+	void AssetManager::InitAssets()
+	{
+		auto paths = FindAllAssetPaths(mainAssetDir);
+		for (auto&& p : paths) {
+			AddAsset(std::make_unique<Asset>(p));
+		}
+	}
+	void AssetManager::AddAsset(std::unique_ptr<Asset> asset)
+	{
+		Logger::Get().BeginSection("Asset detected");
+		Logger::Get().Log("Id: " + uuids::to_string(asset->GetId()));
+		Logger::Get().Log("Name: " + asset->GetName());
+		Logger::Get().EndSection();
+		assets.insert({ asset->GetId(), std::move(asset) });
 	}
 	const Asset& AssetManager::GetAsset(uuids::uuid id) const
 	{
@@ -27,16 +43,5 @@ namespace AssetManagement {
 			}
 		}
 		return result;
-	}
-	void AssetManager::InitAssets()
-	{
-		auto paths = FindAllAssetPaths(mainAssetDir);
-		for (auto&& p : paths) {
-			AddAsset(std::make_unique<Asset>(p));
-		}
-	}
-	void AssetManager::AddAsset(std::unique_ptr<Asset> asset)
-	{
-		assets.insert({ asset->GetId(), std::move(asset) });
 	}
 }
