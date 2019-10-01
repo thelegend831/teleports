86c3ffffbb3413c64ae6ab1f532435081dd1130b
Dominik Kowalczyk <arkenvoss@live.com>
2019-10-01 14:42:42 +0200

    Vertex buffer data update test

--- sisyphus/Renderer/src/renderer_main.cpp
+++ sisyphus/Renderer/src/renderer_main.cpp
@@ -16,31 +16,50 @@ int main() {
 
 		logger->BeginSection("Vulkan Renderer");
 		Vulkan::Renderer renderer(rendererCreateInfo);
-
-		Vulkan::Renderer::UniformBufferData ubData{ 255, 0, 0 };
-		renderer.UpdateUniformBuffer(ubData);
 		logger->EndSection();
-		logger->Log("Uniform Buffer updated with 255, 0, 0\n");
 
-		auto ubDataFromGPU = renderer.GetUniformBufferData();
-		if (memcmp(&ubData, &ubDataFromGPU, sizeof(ubData)) != 0) {
-			throw std::logic_error("Uniform buffer data manipulation error!");
-		}
-
+		// Shaders
 		auto vertexShaderFileId = uuids::uuid::from_string("e1124008-e112-4008-a2f3-cf6233498020").value();
 		String vertexShaderText(assetManager.GetAsset(vertexShaderFileId).GetDataAsString());
 		auto fragmentShaderFileId = uuids::uuid::from_string("ce637e01-1d00-405c-8aaa-f0c022235745").value();
 		String fragmentShaderText(assetManager.GetAsset(fragmentShaderFileId).GetDataAsString());
 
 		renderer.CreateShader(
-			uuids::uuid::from_string("9b8c0852-be27-4c0e-add9-da8e2ccf464f").value(), 
-			vertexShaderText, 
+			uuids::uuid::from_string("9b8c0852-be27-4c0e-add9-da8e2ccf464f").value(),
+			vertexShaderText,
 			ShaderType::Vertex);
 
 		renderer.CreateShader(
 			uuids::uuid::from_string("36b455c9-b2e0-4bae-a89a-8f7fc750ff74").value(),
 			fragmentShaderText,
 			ShaderType::Fragment);
+
+		// Uniform buffer
+		Vulkan::Renderer::UniformBufferData ubData{ 255, 0, 0 };
+		renderer.UpdateUniformBuffer(ubData);
+		logger->Log("Uniform Buffer updated with 255, 0, 0!");
+
+		auto ubDataFromGPU = renderer.GetUniformBufferData();
+		if (memcmp(&ubData, &ubDataFromGPU, sizeof(ubData)) != 0) {
+			throw std::logic_error("Uniform buffer data manipulation error!");
+		}
+
+		// Vertex buffer
+		Vulkan::Renderer::VertexBufferData vbData{
+			{
+				{ 0.2f, 0.2f, 0.f },
+				{ 0.2f, 0.8f, 0.f },
+				{ 0.8f, 0.8f, 0.f },
+				{ 0.8f, 0.2f, 0.f },
+			}
+		};
+		renderer.UpdateVertexBuffer(vbData);
+		logger->Log("Vertex Buffer updated!");
+
+		auto vbDataFromGPU = renderer.GetVertexBufferData();
+		if (memcmp(&vbData, &vbDataFromGPU, sizeof(vbData)) != 0) {
+			throw std::logic_error("Vertex buffer data manipulation error!");
+		}
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
