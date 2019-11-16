d95658c63ad77f26cb1277720c2701081dc8aea9
dkowl <arkenvoss.dk@gmail.com>
2019-11-16 19:10:53 +0100

    Editor using Sisyphus namespace

--- sisyphus/Editor/src/editor_main.cpp
+++ sisyphus/Editor/src/editor_main.cpp
@@ -2,6 +2,8 @@
 #include "Utils/FilesystemUtils.h"
 
 int main() {
+	using namespace Sisyphus;
+
 	Path projectPath = R"(C:\Teleports\teleports\sisyphus\Renderer\project)";
 	Path assetDir = projectPath / "Assets";
 
