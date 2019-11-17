36ae353ab7cca5c9c84524b31c9a01573ce88c13
dkowl <arkenvoss.dk@gmail.com>
2019-11-17 17:42:49 +0100

    IDrawable extended with 4 methods

--- sisyphus/Renderer/include/Renderer/IDrawable.h
+++ sisyphus/Renderer/include/Renderer/IDrawable.h
@@ -2,6 +2,10 @@
 
 namespace Sisyphus::Rendering {
 	class IDrawable {
-
+	public:
+		virtual uint32_t GetVertexCount() const = 0;
+		virtual uint32_t GetVertexStride() const = 0;
+		virtual size_t GetVertexBufferSize() const = 0;
+		virtual std::byte* GetVertexData() const = 0;
 	};
 }
