fb32e9a416921107a08a641a7d15985f568ba4bf
dkowl <arkenvoss.dk@gmail.com>
2019-12-15 13:00:32 +0100

    LogArgs and AssembleString utilities

--- sisyphus/Utils/include/Utils/ILogger.h
+++ sisyphus/Utils/include/Utils/ILogger.h
@@ -1,12 +1,18 @@
 #pragma once
 #include <string>
+#include "Utils/StringUtils.h"
 
 class ILogger {
 public:
 	virtual ~ILogger() = default;
 
-	virtual void Log(const std::string& message, int logLevel = 0) = 0;
+	template<typename... Args>
+	void LogArgs(Args... args) {
+		Log(Sisyphus::AssembleString(std::forward(args...)));
+	}
 
+	virtual void Log(const std::string& message, int logLevel = 0) = 0;
+	
 	virtual void BeginSection(std::string name, std::string indenter = "\t") = 0;
 	virtual void EndSection() = 0;
 
--- sisyphus/Utils/include/Utils/StringUtils.h
+++ sisyphus/Utils/include/Utils/StringUtils.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <string>
+#include <sstream>
 
 namespace Sisyphus {
 	using String = ::std::string;
+
+	template<typename... Args>
+	String AssembleString(Args... args) {
+		std::stringstream ss;
+		(ss << ... << args);
+		return ss.str();
+	}
 }
