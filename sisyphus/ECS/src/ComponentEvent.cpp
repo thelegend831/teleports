7843b2ce799d61ceafc113fc2c41ea0a67004a48
dkowl <arkenvoss.dk@gmail.com>
2019-12-21 13:31:02 +0100

    Initialization event ID generated

--- sisyphus/ECS/src/ComponentEvent.cpp
+++ sisyphus/ECS/src/ComponentEvent.cpp
@@ -1,11 +1,14 @@
 #include "Pch_ECS.h"
 #include "ComponentEvent.h"
+#include "Utils\UuidUtils.h"
 
 namespace Sisyphus::ECS {
 	namespace Events {
+		SIS_DEFINE_ID(ComponentEventID_Initialization, "490cc459066042b6a9f82fad2c2467e7");
+
 		uuids::uuid Initialization::Id()
 		{
-			return uuids::uuid();
+			return ComponentEventID_Initialization;
 		}
 	}
 }
