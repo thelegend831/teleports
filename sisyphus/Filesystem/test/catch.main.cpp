8812028bb6ac5b0fc4753e4b36ba4d2ea4f1a923
dkowl <arkenvoss.dk@gmail.com>
2020-03-01 16:45:59 +0100

    Filesystem project created

---
+++ sisyphus/Filesystem/test/catch.main.cpp
@@ -0,0 +1,18 @@
+#ifdef __ANDROID__
+#define CATCH_CONFIG_RUNNER
+#else
+#define CATCH_CONFIG_MAIN
+#endif
+#include "catch.hpp"
+
+#ifdef __ANDROID__
+#include <jni.h>
+
+extern "C"
+JNIEXPORT JNICALL
+int Java_com_FilesystemAndroidTestApp_FilesystemAndroidTestApp_runTest(JNIEnv * env, jclass type) {
+    const char* argv[] = { "whatever" };
+    int result = Catch::Session().run(1, argv);//fake `argc` and `argv`
+    return result;
+}
+#endif
