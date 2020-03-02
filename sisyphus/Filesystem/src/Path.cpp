e1e72f7f72209ab2653f065f9db60a699ef6562a
dkowl <arkenvoss.dk@gmail.com>
2020-03-02 14:59:07 +0100

    Path filename dirname and extension implemented and tested

--- sisyphus/Filesystem/src/Path.cpp
+++ sisyphus/Filesystem/src/Path.cpp
@@ -24,6 +24,32 @@ namespace Sisyphus::Filesystem {
 		return Path(std::string(filename, filenameLength - extensionLength));
 	}
 
+	Path Path::Filename()
+	{
+		const char* filename;
+		size_t filenameLength;
+		cwk_path_get_basename(pathString.c_str(), &filename, &filenameLength);
+
+		return Path(std::string(filename, filenameLength));
+	}
+
+	Path Path::Dirname()
+	{
+		size_t dirnameLength;
+		cwk_path_get_dirname(pathString.c_str(), &dirnameLength);
+
+		return Path(std::string(pathString.c_str(), dirnameLength));
+	}
+
+	Path Path::Extension()
+	{
+		const char* extension;
+		size_t extensionLength;
+		cwk_path_get_extension(pathString.c_str(), &extension, &extensionLength);
+
+		return Path(std::string(extension, extensionLength));
+	}
+
 	std::string Path::String() {
 		return pathString;
 	}
--- sisyphus/Filesystem/test/Test_Path.cpp
+++ sisyphus/Filesystem/test/Test_Path.cpp
@@ -7,5 +7,7 @@ TEST_CASE("Stem") {
 	Path path("Dir/Dir2/file.cpp");
 
 	REQUIRE(path.Stem().String() == "file");
-
+	REQUIRE(path.Filename().String() == "file.cpp");
+	REQUIRE(path.Dirname().String() == "Dir/Dir2/");
+	REQUIRE(path.Extension().String() == ".cpp");
 }
