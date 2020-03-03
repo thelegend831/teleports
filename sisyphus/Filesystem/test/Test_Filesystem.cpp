40e062fabb71aea3c828e6d8d33faa8f1589017b
dkowl <arkenvoss.dk@gmail.com>
2020-03-03 14:41:19 +0100

    Filesystem test involving file creation, failing on Android

--- sisyphus/Filesystem/test/Test_Filesystem.cpp
+++ sisyphus/Filesystem/test/Test_Filesystem.cpp
@@ -2,6 +2,9 @@
 #include "Filesystem.h"
 #include "Path.h"
 #include "Utils/PlatformMacros.h"
+#include <fstream>
+#include <iostream>
+#include <cstdio>
 
 using namespace Sisyphus;
 
@@ -9,12 +12,40 @@ using namespace Sisyphus;
 #include "Globals.Android.h"
 #endif
 
-TEST_CASE("Exists") {
-	Fs::Path dummyPath("dummy.txt");
-
+TEST_CASE("Filesystem") {
 #ifdef SIS_ANDROID
 	Fs::Init_Android((void*)JavaGlobals::jniEnv, (void*)JavaGlobals::assetManager);
 #endif
 
+	Fs::Path dummyPath("dummy.txt");
+
 	REQUIRE(!Fs::Exists(dummyPath));
+	REQUIRE(!Fs::IsRegularFile(dummyPath));
+	REQUIRE(!Fs::IsDirectory(dummyPath));
+
+	std::fstream file(dummyPath.String(), std::fstream::out);
+	if (file.good()) {
+		std::cout << "File " << dummyPath.String() << " opened.\n";
+	}
+	else {
+		std::cout << "Error opening file " << dummyPath.String() << "\n";
+		REQUIRE(false);
+	}
+	
+	try {
+		REQUIRE(Fs::Exists(dummyPath));
+		REQUIRE(Fs::IsRegularFile(dummyPath));
+		REQUIRE(!Fs::IsDirectory(dummyPath));
+	}
+	catch(...){
+
+	}
+
+	file.close();
+	if( remove(dummyPath.String().c_str()) != 0 ){
+		std::cout << "Error deleting file " << dummyPath.String() << "\n";
+	}
+	else {
+		std::cout << "File " << dummyPath.String() << " deleted.\n";
+	}
 }
