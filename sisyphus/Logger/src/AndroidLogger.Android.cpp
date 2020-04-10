b1e9ac0329c62d34cb186c69c87e06231adc901c
dkowl <arkenvoss.dk@gmail.com>
2020-04-11 01:36:49 +0200

    android: logging level defaults to info

--- sisyphus/Logger/src/AndroidLogger.Android.cpp
+++ sisyphus/Logger/src/AndroidLogger.Android.cpp
@@ -5,6 +5,6 @@ namespace Sisyphus::Logging {
 
 	void AndroidLogger::Output(const std::string& s) {
 		// WISH: map logLevels to interal Android log levels from https://developer.android.com/ndk/reference/group/logging
-		__android_log_write(currentLogLevel, "Sisyphus", s.c_str());
+		__android_log_write(ANDROID_LOG_INFO, "Sisyphus", s.c_str());
 	}
 }
