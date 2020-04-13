08f7148db658ed959daaabafe77e2ecfc206e031
dkowl <arkenvoss.dk@gmail.com>
2020-04-13 21:11:12 +0200

    Utils: StringFromJava

--- sisyphus/Utils/include/Utils/StringUtils.h
+++ sisyphus/Utils/include/Utils/StringUtils.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <string>
 #include <sstream>
+#include "Utils/PlatformMacros.h"
+#ifdef SIS_ANDROID
+#include <jni.h>
+#endif
 
 namespace Sisyphus {
 	using String = ::std::string;
@@ -11,4 +15,8 @@ namespace Sisyphus {
 		(ss << ... << args);
 		return ss.str();
 	}
+
+#ifdef SIS_ANDROID
+	std::string StringFromJava(JNIEnv* env, jstring jStr);
+#endif
 }
