532351e0f6aa28949b738d68c4cfc6fe4d29c2ac
dkowl <arkenvoss.dk@gmail.com>
2020-05-11 19:01:47 +0200

    Editor: now using the build system

--- sisyphus/Editor/src/editor_main.cpp
+++ sisyphus/Editor/src/editor_main.cpp
@@ -1,11 +1,12 @@
 #include "AssetManagement/AssetReader.h"
 #include "Utils/FilesystemUtils.h"
 #include <iostream>
+#include "CLI11.hpp"
 
 int main() {
 	using namespace Sisyphus;
 
-	Path projectPath = R"(C:\Repos\teleports\sisyphus\Projects\TestProject\Windows\project)";
+	Path projectPath = R"(C:\Repos\teleports\sisyphus\Projects\TestProject)";
 	Path assetDir = projectPath / "Assets";
 
 	auto assetReader = AssetManagement::AssetReader::Create(AssetManagement::AssetReader::ReaderType::Unpacked);
