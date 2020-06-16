a84699c0d5fb5634b1d0299562f6ba491c3a6ede
dkowl <arkenvoss.dk@gmail.com>
2020-06-16 14:02:52 +0200

    Demo3D project

---
+++ sisyphus/Projects/Demo3D/src/Demo3D.main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "AssetManagement/AssetReader.h"
+#include "Renderer/RendererFactory.h"
+#include "WindowCreator/WindowCreator.h"
+
+using namespace Sisyphus;
+
+int main() {
+
+	std::cout << "Hello World!" << std::endl;
+}
